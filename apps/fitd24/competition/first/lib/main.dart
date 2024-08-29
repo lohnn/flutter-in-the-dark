991bc4f4833d7d46085017140512e735e3fc83b9
lohnn <johannes@lohnn.se>
2024-08-29 20:31:15 +0200

    Setting up blueprint projects for the challenges

---
+++ apps/fitd24/competition/first/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
+
+const color1 = Color(0xff3c2b79);
+const color2 = Color(0xff594a8f);
