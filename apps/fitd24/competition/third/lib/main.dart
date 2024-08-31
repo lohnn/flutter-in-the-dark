56b7b25ee9747aeb1bbc65fe43fd46d515e5d475
lohnn <johannes@lohnn.se>
2024-08-31 11:02:59 +0200

    Help with setting dark theme for third

--- apps/fitd24/competition/third/lib/main.dart
+++ apps/fitd24/competition/third/lib/main.dart
@@ -11,10 +11,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData.dark(),
+      home: const Scaffold(
+        body: FrostedBackground(
+          image: dash,
         ),
       ),
     );
